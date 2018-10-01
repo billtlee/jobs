ee52b2cc74a743de1550aaab84f91acd66860838
billtlee <wlee3@hotmail.com>
2018-10-01 10:42:23 +0800

    add welcomescreen, reviewscreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  { text: 'Welcome to JobApp', color: '#03A9F4' },
+  { text: 'Use this to get a job', color: '#009688' },
+  { text: 'Set your location, then swipe away', color: '#03A9F4' }
+];
 
 class WelcomeScreen extends Component {
+  onSlidesComplete = () => {
+    this.props.navigation.navigate('auth');
+  }
+
   render() {
     return (
-      <View>
-        <Text>WelcomeScreen</Text>
+      <View
+        style={{ flex: 1 }}
+      >
+        <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />
       </View>
     );
   }
